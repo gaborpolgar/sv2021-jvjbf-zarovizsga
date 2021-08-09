9b5989bf12afadad7cb043b09fc691120e1bf032
Gábor Polgár <56192725+gaborpolgar@users.noreply.github.com>
2021-08-09 13:48:37 +0200

    Initial commit

---
+++ src/main/java/finalexam/Sv2021JvjbfZarovizsgaApplication.java
@@ -0,0 +1,20 @@
+package finalexam;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Sv2021JvjbfZarovizsgaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sv2021JvjbfZarovizsgaApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelmapper() {
+        return new ModelMapper();
+    }
+
+}
